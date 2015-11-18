0f6a3140b6525ba8ba1919ac10fb439b48c1e469
SUDA Hiroshi <suda@net.it-chiba.ac.jp>
2015-11-18 17:26:34 +0900

    iの変数宣言が抜けていたので追加

--- exSA02.java
+++ exSA02.java
@@ -4,7 +4,7 @@
  public class exSA02 {
    public static void main( String[] args ) {
      SuperArray sa = new SuperArray( 30 );
-     for( i=0; i<30; i++ ) {
+     for( int i=0; i<30; i++ ) {
        sa.push( i );
      }
     System.out.println( sa );
